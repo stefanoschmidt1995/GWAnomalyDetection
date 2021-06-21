8fe07e2275f3b600ac38f337494fde8743e69106
stefanoschmidt1995 <stefanoschmidt1995@gmail.com>
2021-06-21 18:09:36 +0200

    Tiny fix in make_injs

--- maxent/make_injs.py
+++ maxent/make_injs.py
@@ -5,11 +5,13 @@ import os
 
 import mlgw.GW_generator as gen
 
-	#creating 30 injs
-injs = create_injection_list(30, 4096, 1126257415, 1126257415+100, datafile = 'data/H-H1_GWOSC_4KHZ_R1-1126257415-100.txt.gz')
+N_injs = 30 #number of injections
+N_batch = 10
+
+injs = create_injection_list(N_injs, 4096, 1126257415, 1126257415+100, datafile = 'data/H-H1_GWOSC_4KHZ_R1-1126257415-100.txt.gz')
 
 split_data('data/H-H1_GWOSC_4KHZ_R1-1126257415-100.txt.gz', outfolder = 'data_temp',
-			 srate = 4096, start_GPS = 1126257415, T_batch = 30, T_overlap = 10, 
+			 srate = 4096, start_GPS = 1126257415, T_batch = 100/N_batch, T_overlap = 0., 
 			 injection_list = injs,
 			 downsampling_factor = 4, #for some reason, the non downsampled data are not suitable for emd...
 			 prefix = 'H',
