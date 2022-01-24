0ce2f0503943fd4af2cdab795345cfce0cc82e4b
Robin van der Laag <94386728+RvdLaag@users.noreply.github.com>
2022-01-24 16:26:53 +0100

    Update aux_fracdim.py

--- wp5/aux/aux_fracdim.py
+++ wp5/aux/aux_fracdim.py
@@ -9,22 +9,24 @@ from numba import cuda
 from tqdm import tqdm
 import gc
 
+import time
+
 # Select GPU to use | same order as nvidia-smi
-cuda.select_device(0)
+cuda.select_device(3)
 
 # ANAM CUDA kernel
-@cuda.jit("void(float32[:,:], float32[:,:,:])")
-def ANAM(f, A):
+@cuda.jit("void(float32[:,:], float32[:,:], int16)")
+def ANAM(f, A, dec):
     x, tau, i = cuda.grid(3)
     tau += 1
     n = f.shape[1]
-    if x < A.shape[0] and tau < A.shape[1]+1 and i < n-tau and i >= tau:
+    if x < A.shape[0] and tau < dec+1 and i < n-tau and i >= tau:
         out = float(0)
         denom = (tau+1)**2/(n-2*tau)
         for j in range(0, tau+1):
             for l in range(0, tau+1):
                 out += abs(f[x,i+j]-f[x,i-l])
-        A[x, tau-1, i] =  out/denom
+        A[x, i-tau + (n-1)*(tau-1)-(tau-1)**2] =  out/denom
 
 
 
@@ -34,7 +36,6 @@ keys = ['256Hz', '512Hz', '1024Hz', '2048Hz', '4096Hz']
 
 desired_segment_time = 0.2 # in seconds | should match desired_segment_time from aux_segments.py
 secs_per_seg = np.ceil(256*desired_segment_time)/256 # time in seconds per segment 
-segs_per_run = 36 # number of segments the kernel can handle per run | should be changed accordingly if number of channels changes
 
 for file in tqdm(files, desc='Files'):
     with h5py.File(f'./{file}', 'r') as f:
@@ -44,39 +45,13 @@ for file in tqdm(files, desc='Files'):
         meta_group = g.create_group('meta')
         meta_group.create_dataset('Start GPS', data=int(start_t))
         meta_group.create_dataset('End GPS', data=int(end_t))
-        
+    
+    
     # Loop over different sampling rates
     # The sampling rates determines the shape of the data segments and requires a different kernel size
     for key in tqdm(keys, desc='Sample Rates', leave=False):
         sample_rate = int(key[:-2])
-        dec = min(32, int(sample_rate*secs_per_seg/2)) # decimate factor | minimum of 32 should be changed together with 'segs_per_run'
-        
-        # values used to determine the slope using linear regression later
-        log_taus = np.log(np.arange(1,dec))
-        lin_regress_denom = ((log_taus**2).mean() - (log_taus.mean())**2)
-        
-        # number of threads per block | product limited to 1024 (depends on gpu)
-        threadsperblock = (4, 4, 64)
-        
-        '''
-        TODO: FIX blockpergrid
-            Currently any higher values than these will result in the error: 
-            CudaAPIError: [1] Call to cuLaunchKernel results in CUDA_ERROR_INVALID_VALUE
-            However, each value is significantly smaller than the limit of 65535.
-            
-            Thread asking for help/clarification has been openened:
-                https://forums.developer.nvidia.com/t/cudaapierror-1-call-to-culaunchkernel-results-in-cuda-error-invalid-value-with-numba/200699
-            But no reply as of 2022/01/19
-            
-            Currently values are the highest they can be without causing the error
-            If a different time bin length (secs_per_seg) is chosen the values of blockspergrid will
-            have to be redetermined. 
-            Easiest way would be to manually change 'segs_per_run' to be as high as possible without the error occuring
-        '''
-        # numbers of block per grid | should be less than 65535 in each dimension
-        blockspergrid = ((segs_per_run + (threadsperblock[0] - 1)) // threadsperblock[0],
-                         (dec + (threadsperblock[1] - 1)) // threadsperblock[1],
-                         (int(sample_rate*secs_per_seg) + (threadsperblock[2] - 1)) // threadsperblock[2])
+        dec = min(32, int(sample_rate*secs_per_seg/2)-1) # decimate factor | minimum of 32 should be changed together with 'segs_per_run'
         
         # read data from segment filee
         with h5py.File(f'./{file}', 'r') as f:
@@ -85,23 +60,35 @@ for file in tqdm(files, desc='Files'):
 
         shape = data_all.shape
         
-        '''
-        TODO: transfer output array to gpu manually could improve performance
-            initial attempts to do this ran into out of memory errors as the output array can be greater than 10Gb
-            
-        '''
+        # values used to determine the slope using linear regression later
+        log_taus = np.log(np.arange(1,dec+1))
+        lin_regress_denom = ((log_taus**2).mean() - (log_taus.mean())**2)
+        
+        # number of threads per block | product limited to 1024 (depends on gpu)
+        threadsperblock = (4, 4, 64)
+        
+        # numbers of block per grid | should be less than 65535 in each dimension
+        blockspergrid = ((shape[1] + (threadsperblock[0] - 1)) // threadsperblock[0],
+                         (dec + (threadsperblock[1] - 1)) // threadsperblock[1],
+                         (shape[2] + (threadsperblock[2] - 1)) // threadsperblock[2])
+        
+        mask = np.arange(1,dec+1)
+        mask = ((shape[2]-1)*(mask-1)-(mask-1)**2).astype(int)
+        
         outsums = []
-        for j, (data, name) in enumerate(zip(data_all, names)):
+        timer_start = time.perf_counter()
+        for data, name in zip(data_all, names):
             data = cuda.to_device(data)
-            out = np.zeros((shape[1], dec-1, shape[2]), dtype=data_all.dtype)
-            for i in range(int(shape[1]/segs_per_run+1)):
-                ANAM[threadsperblock, blockspergrid](data[i*segs_per_run:(i+1)*segs_per_run], 
-                                                     out[i*segs_per_run:(i+1)*segs_per_run])
-            outsums.append(np.sum(out, axis=2)) 
+            out = np.zeros((shape[1], (shape[2]-1)*(dec)-(dec)**2), dtype=data_all.dtype)
+            ANAM[blockspergrid,threadsperblock](data, out, dec)
+            # synchronize gpu to ensure all tasks are completed
+            #cuda.synchronize()
+            outsums.append(np.add.reduceat(out, mask, axis=1))
         cuda.synchronize() # synchronize gpu to ensure all tasks are completed | not sure if necessary
-        
-        
+        tqdm.write(f'{key} | {time.perf_counter()-timer_start} seconds')
+        #print(key, ':', time.perf_counter()-timer_start, ' s')
         out = np.log(np.asarray(outsums)) 
+        
         # vectorized fractal dimension calculation using slope linear regression
         frac_dims = (2-((log_taus*out).mean(axis=2)-log_taus.mean()*out.mean(axis=2))/lin_regress_denom) 
         
@@ -111,8 +98,3 @@ for file in tqdm(files, desc='Files'):
                 g['data'][name].attrs['Sample rate'] = int(key[:-2])
         
         del data_all, frac_dims, out, data # free memory of no longer used data
-        
-    
-        
-            
-
