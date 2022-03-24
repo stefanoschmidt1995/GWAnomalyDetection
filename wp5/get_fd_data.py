1bddacb51c6bdff12f00c725278ac42f3a7fa319
Robin van der Laag <94386728+RvdLaag@users.noreply.github.com>
2022-03-24 09:28:19 +0100

    Added support for synthetic data
    Can now calculate the FD of (synthetic) data from .hdf files generated by PyCBC (temporary hack, TODO: allow input of data as numpy array)

--- wp5/get_fd_data.py
+++ wp5/get_fd_data.py
@@ -10,13 +10,45 @@ import pathlib
 from numba import cuda, float32, int16
 
 import argparse
+
+from tqdm.auto import tqdm
+
+##################################################################
+'''
+cvmfs/oasis.opensciencegrid.org/ligo/sw/conda/envs/igwn-py38/lib/python3.8/site-packages/numba/cuda/cudadrv/devicearray.py:790: NumbaPerformanceWarning: Host array used in CUDA kernel will incur copy overhead to/from device.
+'''
+# TODO: investigate for performance increase
+
+# Ignore warning for now
+from numba.core.errors import NumbaPerformanceWarning
+import warnings
+warnings.simplefilter('ignore', category=NumbaPerformanceWarning)
+##################################################################
     
-def main(strain_channel, whiten, t_start, t_end, segment_time, overlap, estimator, decimate_factor, gpu, out_dir='FD/'):
+def get_fd(strain_channel, whiten, t_start, t_end, segment_time, overlap, estimator, decimate_factor, gpu, out_dir='FD/'):
+    
+    if isinstance(segment_time,(list,tuple,np.ndarray)):
+        if len(segment_time) > 1:
+            multi_segs = True
+        else:
+            multi_segs = False
+    else:
+        multi_segs = False
+        segment_time = [segment_time]
+    segment_time = np.asarray(segment_time)
+    
+    if strain_channel.split('.')[-1] == 'hdf':
+        strain_file = strain_channel if strain_channel[:2]=='./' else './'+strain_channel
+        strain_channel = 'Synthetic_Data'
+        
     str_overlap = f'-o{str(overlap).replace(".","_")}' if overlap > 0 else ''
-    whiten_overlap = f'-whitened' if whiten else ''
+    str_whiten = f'-w' if whiten else ''
     out_dir = f'{out_dir}{t_start}-{t_end}/'
+    str_segtime = 'multi' if multi_segs else f'{str(segment_time[0]).replace(".","_")}s{str_overlap}'
     pathlib.Path(out_dir).mkdir(parents=True, exist_ok=True) 
-    save_file = f'./{out_dir}/{estimator}-{strain_channel.split(":")[0]}-{str(segment_time).replace(".","_")}s{str_overlap}{whiten_overlap}.hdf5' # path for save file
+    save_file = f'./{out_dir}/{estimator}-{strain_channel.split(":")[0]}-{str_segtime}{str_whiten}.hdf5' # path for save file
+    
+    cuda.select_device(gpu)
     
     if estimator == 'VAR':
         # VAR CUDA kernel
@@ -55,7 +87,7 @@ def main(strain_channel, whiten, t_start, t_end, segment_time, overlap, estimato
     else:
         raise ValueError("Incorrect value for: estimator. Choose from {VAR, ANAM}.")
     
-    cuda.select_device(gpu)
+    
     context = cuda.current_context()
     free_mem = context.get_memory_info().free - 10*2**20
     
@@ -64,11 +96,21 @@ def main(strain_channel, whiten, t_start, t_end, segment_time, overlap, estimato
     threadsperblock = (4, 4, 64)
     assert np.prod(threadsperblock) <= MAX_THREADS_PER_BLOCK, f'Exceeded maximum threads per block on currect device: {gpu.name}'
     
-    segment_time = math.ceil(segment_time*256)/256
-    
     #####
-    strain_data = gwpy.timeseries.TimeSeries.get(strain_channel, t_start, t_end)
-    strain_sample_rate = strain_data.sample_rate.value
+    if strain_channel == 'Synthetic_Data':
+        # hdf file with data
+        assert whiten == False, 'Whiten not supported for hdf file' # TODO: support?
+        with h5py.File(strain_file, 'r') as f:
+            strain_data = f['data'][:]
+            strain_sample_rate = 1/f['data'].attrs['delta_t'][()]
+    elif strain_channel[:4].lower() == 'open':
+        # open data
+        ifo = strain_channel[4:]
+        strain_data = gwpy.timeseries.TimeSeries.fetch_open_data(ifo, t_start, t_end)
+        strain_sample_rate = strain_data.sample_rate.value
+    else:
+        strain_data = gwpy.timeseries.TimeSeries.get(strain_channel, t_start, t_end)
+        strain_sample_rate = strain_data.sample_rate.value
     
     if whiten:
         fduration = 2 # default
@@ -78,105 +120,128 @@ def main(strain_channel, whiten, t_start, t_end, segment_time, overlap, estimato
         t_end = strain_data.times[-1].value-0.5*fduration
         strain_data = strain_data.crop(t_start, t_end)
         
+    segment_time = (np.ceil(segment_time*256)/256 * strain_sample_rate).astype(int)
+    segment_time[np.where((segment_time & 1)==0)] -= 1 # subtract one if seg_time is even
+    segment_time[::-1].sort() # sorts array into descending order
+    overlap *= strain_sample_rate
     
     # TODO: change to float32 for speed increase (conversion takes time) at the cost of some precision (worth it?)
-    strain_data = strain_data.value #.astype(np.float32)
-    
-    data_strides = strain_data.strides
-    seg_length = int(segment_time * strain_sample_rate) # number of points per segment
-    overlap_length = int(seg_length*overlap) # number of points that overlap with next segment
-    num_segs = int( (strain_data.shape[0] - overlap_length)/(seg_length - overlap_length) ) # number of segments
+    try:
+        strain_data = strain_data.value #.astype(np.float32)
+    except AttributeError: #  isinstance(gwpy.Timeseries, np.ndarray)=True, so can't filter using this. Catching AttributeError instead 
+        assert isinstance(strain_data, np.ndarray)
+        pass
+    for seg_time in tqdm(segment_time, leave=False):
+        data = strain_data[int(segment_time[0]/2-seg_time/2):] # slice strain_data (view) such that all segments start with same point in center
+        ov = seg_time - (segment_time[-1]-overlap)
+        
+        data_strides = strain_data.strides
+        seg_length = int(seg_time) # number of points per segment
+        overlap_length = int(ov) # number of points that overlap with next segment
+        num_segs = int( (data.shape[0] - overlap_length)/(seg_length - overlap_length) ) # number of segments
 
-    seg_shape = (num_segs, seg_length)
-    seg_strides = (data_strides[0]*(seg_length-overlap_length), data_strides[0])
+        seg_shape = (num_segs, seg_length)
+        seg_strides = (data_strides[0]*(seg_length-overlap_length), data_strides[0])
 
 
-    # TODO: explore rewriting kernel to accept unsegmented strain and use slicing to avoid having to segment here
-    if overlap == 0:
-        strain_segs = np.lib.stride_tricks.as_strided(strain_data, shape=seg_shape, strides=seg_strides)
-    else:
-        # contiguous data is needed to transfer to gpu
-        strain_segs = np.ascontiguousarray(np.lib.stride_tricks.as_strided(strain_data, shape=seg_shape, strides=seg_strides))
-    
-    NV = seg_length//(2*decimate_factor)
-    
-    # Array to store outputs of the estimators.
-    strain_ests = np.empty((num_segs,NV), dtype=strain_segs.dtype)
-    
-    # used for calculating slope
-    log_taus = np.log(np.arange(1,NV+1))
-    lin_regress_denom = ((log_taus**2).mean() - (log_taus.mean())**2)
-    
-    if estimator == 'VAR':
-        finfo = np.finfo(strain_segs.dtype)
-        minmax_dtype = np.array([finfo.min, finfo.max])
-        denom = seg_length-2*np.arange(1,NV+1)
-    elif estimator == 'ANAM':
-        denom = np.arange(1,NV+1)
-        denom = (out_denom+1)**2 * (seg_length - 2*out_denom)
-    
-    # TODO: max of 65535 as gpu variable
-    block_limit = min(int(free_mem/(strain_data.itemsize*(seg_length + NV))), 65535) 
-    
-    if num_segs > block_limit:
-        for i in range(int(num_segs/block_limit)+1):
-            left_idx = i*block_limit
-            right_idx = min((i+1)*(block_limit), num_segs)
-            idx_length = right_idx-left_idx
+        # TODO: explore rewriting kernel to accept unsegmented strain and use slicing to avoid having to segment here
+        if ov == 0:
+            strain_segs = np.lib.stride_tricks.as_strided(data, shape=seg_shape, strides=seg_strides)
+        else:
+            # contiguous data is needed to transfer to gpu
+            strain_segs = np.ascontiguousarray(np.lib.stride_tricks.as_strided(data, shape=seg_shape, strides=seg_strides))
 
+        NV = seg_length//(2*decimate_factor)
+
+        # Array to store outputs of the estimators.
+        strain_ests = np.empty((num_segs,NV), dtype=strain_segs.dtype)
+
+        # used for calculating slope
+        log_taus = np.log(np.arange(1,NV+1))
+        lin_regress_denom = ((log_taus**2).mean() - (log_taus.mean())**2)
+
+        if estimator == 'VAR':
+            finfo = np.finfo(strain_segs.dtype)
+            minmax_dtype = np.array([finfo.min, finfo.max])
+            denom = seg_length-2*np.arange(1,NV+1)
+        elif estimator == 'ANAM':
+            denom = np.arange(1,NV+1)
+            denom = (out_denom+1)**2 * (seg_length - 2*out_denom)
+
+        # TODO: max of 65535 as gpu variable
+        block_limit = min(int(free_mem/(strain_data.itemsize*(seg_length + NV))), 65535) 
+
+        if num_segs > block_limit:
+            for i in range(int(num_segs/block_limit)+1):
+                left_idx = i*block_limit
+                right_idx = min((i+1)*(block_limit), num_segs)
+                idx_length = right_idx-left_idx
+
+                # Create output array
+                out = np.zeros((idx_length, NV), dtype=strain_segs.dtype)
+
+                if estimator == 'VAR':
+                    blockspergrid = ((idx_length + (threadsperblock[0] - 1)) // threadsperblock[0],
+                                     (NV+1 + (threadsperblock[1] - 1)) // threadsperblock[1],
+                                     (seg_length + (threadsperblock[2] - 1)) // threadsperblock[2])
+                    kernel[blockspergrid,threadsperblock](strain_segs[left_idx:right_idx], out, NV, minmax_dtype)
+                elif estimator == 'ANAM':
+                    blockspergrid = ((idx_length + (threadsperblock[0] - 1)) // threadsperblock[0],
+                                     (NV + (threadsperblock[1] - 1)) // threadsperblock[1],
+                                     (seg_length + (threadsperblock[2] - 1)) // threadsperblock[2])
+                    kernel[blockspergrid,threadsperblock](strain_segs[left_idx:right_idx], out, NV)
+
+                strain_ests[left_idx:right_idx] = out/denom
+        else:     
             # Create output array
-            out = np.zeros((idx_length, NV), dtype=strain_segs.dtype)
-            
+            out = np.zeros((num_segs, NV), dtype=strain_segs.dtype)
+
             if estimator == 'VAR':
-                blockspergrid = ((idx_length + (threadsperblock[0] - 1)) // threadsperblock[0],
+                blockspergrid = ((num_segs + (threadsperblock[0] - 1)) // threadsperblock[0],
                                  (NV+1 + (threadsperblock[1] - 1)) // threadsperblock[1],
                                  (seg_length + (threadsperblock[2] - 1)) // threadsperblock[2])
-                kernel[blockspergrid,threadsperblock](strain_segs[left_idx:right_idx], out, NV, minmax_dtype)
+
+                kernel[blockspergrid,threadsperblock](strain_segs, out, NV, minmax_dtype)
             elif estimator == 'ANAM':
-                blockspergrid = ((idx_length + (threadsperblock[0] - 1)) // threadsperblock[0],
+                blockspergrid = ((num_segs + (threadsperblock[0] - 1)) // threadsperblock[0],
                                  (NV + (threadsperblock[1] - 1)) // threadsperblock[1],
                                  (seg_length + (threadsperblock[2] - 1)) // threadsperblock[2])
-                kernel[blockspergrid,threadsperblock](strain_segs[left_idx:right_idx], out, NV)
+
+                kernel[blockspergrid,threadsperblock](strain_segs, out, NV)
+
+            strain_ests[:] = out/denom
+
+        cuda.synchronize()
+
+        # Compute fractal dimensions
+        strain_ests = np.log(strain_ests)
+        strain_frac_dims = 2 - ((log_taus*strain_ests).mean(axis=1) - log_taus.mean()*strain_ests.mean(axis=1))/lin_regress_denom
+
+
+        with h5py.File(save_file, 'a') as f:
+            if 'meta' not in f:
+                f.create_group('meta')
+                f['meta'].create_dataset('Source Channel', data=strain_channel)
+                f['meta'].create_dataset('Source Sample Rate', data=strain_sample_rate)
+                f['meta'].create_dataset('Start GPS', data=t_start+int(segment_time[0]/2)/strain_sample_rate)
+                f['meta'].create_dataset('Delta T', data=(segment_time[-1]-overlap)/strain_sample_rate)
+                #f['meta'].create_dataset('Segment Length', data=segment_time)
+                #f['meta'].create_dataset('Overlap', data=overlap)
+                f['meta'].create_dataset('Whitened', data=whiten)
+                f['meta'].create_dataset('Num Segments', data=num_segs)
+            if 'data' not in f:
+                f.create_group('data')
             
-            strain_ests[left_idx:right_idx] = out/denom
-    else:     
-        # Create output array
-        out = np.zeros((num_segs, NV), dtype=strain_segs.dtype)
-        
-        if estimator == 'VAR':
-            blockspergrid = ((num_segs + (threadsperblock[0] - 1)) // threadsperblock[0],
-                             (NV+1 + (threadsperblock[1] - 1)) // threadsperblock[1],
-                             (seg_length + (threadsperblock[2] - 1)) // threadsperblock[2])
-
-            kernel[blockspergrid,threadsperblock](strain_segs, out, NV, minmax_dtype)
-        elif estimator == 'ANAM':
-            blockspergrid = ((num_segs + (threadsperblock[0] - 1)) // threadsperblock[0],
-                             (NV + (threadsperblock[1] - 1)) // threadsperblock[1],
-                             (seg_length + (threadsperblock[2] - 1)) // threadsperblock[2])
-
-            kernel[blockspergrid,threadsperblock](strain_segs, out, NV)
-
-        strain_ests[:] = out/denom
-    
-    cuda.synchronize()
-    
-    # Compute fractal dimensions
-    strain_ests = np.log(strain_ests)
-    strain_frac_dims = 2 - ((log_taus*strain_ests).mean(axis=1) - log_taus.mean()*strain_ests.mean(axis=1))/lin_regress_denom
-    
-    
-    with h5py.File(save_file, 'w') as f:
-        f.create_group('meta')
-        f['meta'].create_dataset('Strain', data=strain_channel)
-        f['meta'].create_dataset('Strain Sample Rate', data=strain_sample_rate)
-        f['meta'].create_dataset('Start GPS', data=t_start)
-        f['meta'].create_dataset('End GPS', data=t_end)
-        f['meta'].create_dataset('Segment Length', data=segment_time)
-        f['meta'].create_dataset('Overlap', data=overlap)
-        f['meta'].create_dataset('Whitened', data=whiten)
-
-        f.create_group('data')
-        f['data'].create_dataset('FD', data=strain_frac_dims)
+            # longer segment_time can lead to less total segments, 
+            # so only save segments that have data for each segment_time
+            length = f['meta']['Num Segments'][()] 
+            
+            # testing
+            assert num_segs >= length, 'first num_segs is not smallest!'
+            ds = f['data'].create_dataset(f'FD-{str(round(seg_time/strain_sample_rate,2)).replace(".","_")}-{str(round(overlap/strain_sample_rate, 2)).replace(".","_")}', 
+                                          data=strain_frac_dims[:length])
+            ds.attrs['Segment Length'] = segment_time#/strain_sample_rate
+            ds.attrs['Overlap Length'] = overlap#/strain_sample_rate
 
         
         
@@ -187,15 +252,15 @@ if __name__ == "__main__":
     # required arguments
     parser.add_argument('start', type=int, help='GPS Start time')
     parser.add_argument('end', type=int, help='GPS End time')
-    parser.add_argument('segtime', type=float, help='Length of segments in seconds')
+    parser.add_argument('segtime', nargs='+', type=float, help='Length of segments in seconds (can pass multiple)')
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('--var', action='store_true', help='Use the VAR estimator')
     group.add_argument('--anam', action='store_true', help='Use the ANAM estimator')
     
     # optional arguments (defaults are set)
-    parser.add_argument('-c','--channel', type=str, help='Channel to get strain data from', default='L1:DCS-CALIB_STRAIN_C01')
+    parser.add_argument('-c','--channel', type=str, help='Channel to get strain data from, pass "open{ifo}" to use open data', default='L1:DCS-CALIB_STRAIN_C01')
     parser.add_argument('-w','--whiten', action='store_true', help='Whiten strain data')
-    parser.add_argument('-o','--overlap', type=float, help='Fraction of segment time to overlap with previous segment', default=0.0)
+    parser.add_argument('-o','--overlap', type=float, help='Number of seconds to overlap with previous segment (for shortest segtime)', default=0.0)
     parser.add_argument('-d','--dec', type=int, help='Decimate factor for the fractal dimension estimator', default=32)
     parser.add_argument('-g','--gpu', type=int, help='GPU to use', choices=[i for i in range(len(cuda.gpus))], default=0)
     parser.add_argument('-f', '--outdir', type=str, help='Output directory', default='FD/')
@@ -207,4 +272,4 @@ if __name__ == "__main__":
     
     estimator = 'VAR' if args.var else 'ANAM'
     
-    main(args.channel, args.whiten, args.start, args.end, args.segtime, args.overlap, estimator, args.dec, args.gpu, args.outdir)
+    get_fd(args.channel, args.whiten, args.start, args.end, args.segtime, args.overlap, estimator, args.dec, args.gpu, args.outdir)
