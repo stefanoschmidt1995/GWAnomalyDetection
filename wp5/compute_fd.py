2e76489c1bbdf6ca666b53af2c7d0e00b2926430
Robin van der Laag <94386728+RvdLaag@users.noreply.github.com>
2022-10-20 13:27:13 +0200

    Optimized implementation
    Optimized memory use by only using one array to track max and min.
    Included numba signatures in njit call for speedup; default functions are for float64 data, and _f4 functions for float32

--- wp5/compute_fd.py
+++ wp5/compute_fd.py
@@ -1,11 +1,12 @@
 import numpy as np
 import numba
 
-@numba.jit(nopython=True, parallel=True)
-def compute_fd_parallel(fs, dec=64, k_func=None):
+@numba.njit("f8[:](f8[:,:], i4)", parallel=True)
+def compute_fd_parallel(fs, dec, step):
     """
     Computes the Fractal Dimension using the VAR method 
-    and `pyramid summing`.
+    and `pyramid summing` for double-precision (float64) data.
+    For single-precision (float32) data use compute_fd_parallel_f4/compute_fd_serial_f4.
 
     Parameters
     ----------
@@ -24,40 +25,85 @@ def compute_fd_parallel(fs, dec=64, k_func=None):
         Array of computed Fractal Dimension, shape=(num,).
 
     """
-    FDs = np.empty(shape=fs.shape[0])
+    FDs = np.empty(shape=fs.shape[0], dtype=np.float64)
+    N = fs.shape[1]
+    k_n = np.arange(1, N//(2*dec), 1, np.int64)
+    n_max = len(k_n)
     for ii in numba.prange(fs.shape[0]):
         f = fs[ii]
-        N = f.shape[0]
+        V_i = np.empty(shape=(n_max), dtype=np.float64)
         
-        k_n = np.arange(1, N//(2*dec)) if k_func is None else k_func(N, dec)
-        n_max = len(k_n)
-
-        V_i = np.empty(shape=(n_max))
-
-        u_nk = np.empty(shape=(N-2*k_n[0])) # current iteration
-        b_nk = np.empty(shape=(N-2*k_n[0])) # current iteration
-        u_nl = np.empty(shape=(N-2*k_n[0])) # previous iteration
-        b_nl = np.empty(shape=(N-2*k_n[0])) # previous iteration
+        ub = np.empty(shape=(N-2*k_n[0],2), dtype=np.float64) # current iteration
         for i in range(0, N-2*k_n[0]):
-            u_nk[i] = np.max(f[i:i+2*k_n[0]+1])
-            b_nk[i] = np.min(f[i:i+2*k_n[0]+1])
-        V_i[0] = np.mean(u_nk-b_nk)
+            ub[i,0] = np.max(f[i:i+2*k_n[0]+1])
+            ub[i,1] = np.min(f[i:i+2*k_n[0]+1])
+        V_i[0] = np.mean(ub[:,0]-ub[:,1])
 
         for n in range(1,n_max):
-            u_nl[:] = u_nk
-            b_nl[:] = b_nk
             d = k_n[n] - k_n[n-1]
             for i in range(0, N-2*k_n[n]):
-                u_nk[i] = max(u_nl[i], u_nl[i+2*d])
-                b_nk[i] = min(b_nl[i], b_nl[i+2*d])
-            V_i[n] = np.mean(u_nk[:N-2*k_n[n]]-b_nk[:N-2*k_n[n]])
+                ub[i,0] = max(ub[i,0], ub[i+2*d,0])
+                ub[i,1] = min(ub[i,1], ub[i+2*d,1])
+            V_i[n] = np.mean(ub[:N-2*k_n[n],0]-ub[:N-2*k_n[n],1])
 
         X = np.log(k_n)
-        X_m = np.mean(X)
+        X_m = X - np.mean(X)
         Y = np.log(V_i)
-        Y_m = np.mean(Y)
-        FDs[ii] = 2- np.sum((X-X_m)*(Y-Y_m))/np.sum((X-X_m)**2)
+        Y_m = Y - np.mean(Y)
+        FDs[ii] = 2 - np.sum((X_m)*(Y_m))/np.sum((X_m)**2)
     return FDs
 
-compute_fd_serial = numba.jit(compute_fd_parallel.py_func, parallel=False)
+@numba.njit("f4[:](f4[:,:], i4)", parallel=True)
+def compute_fd_parallel_f4(fs, dec, step):
+    """
+    Computes the Fractal Dimension using the VAR method 
+    and `pyramid summing` for single-precision (float32) data.
+    For double-precision (float64) data use compute_fd_parallel/compute_fd_serial.
 
+    Parameters
+    ----------
+    fs : numpy.ndarray
+        2D numpy array of the data, shape=(num, N).
+    dec : int, optional
+        Decimate value for standard k_func. The default is 64.
+    k_func : numba.core.registry.CPUDispatcher, optional
+        Function that takes N, dec and returns a numpy.ndarray for k_n, 
+        must be decorated by @numba.jit(nopython=True).
+        By default it is set to use numpy.arange(1, N//(2*dec)) for k_n.
+
+    Returns
+    -------
+    FDs : numpy.ndarray
+        Array of computed Fractal Dimension, shape=(num,).
+
+    """
+    FDs = np.empty(shape=fs.shape[0], dtype=np.float32)
+    N = fs.shape[1]
+    k_n = np.arange(1, N//(2*dec), 1, np.int32)
+    n_max = len(k_n)
+    for ii in numba.prange(fs.shape[0]):
+        f = fs[ii]
+        V_i = np.empty(shape=(n_max), dtype=np.float32)
+        
+        ub = np.empty(shape=(N-2*k_n[0],2), dtype=np.float32) # current iteration
+        for i in range(0, N-2*k_n[0]):
+            ub[i,0] = np.max(f[i:i+2*k_n[0]+1])
+            ub[i,1] = np.min(f[i:i+2*k_n[0]+1])
+        V_i[0] = np.mean(ub[:,0]-ub[:,1])
+
+        for n in range(1,n_max):
+            d = k_n[n] - k_n[n-1]
+            for i in range(0, N-2*k_n[n]):
+                ub[i,0] = max(ub[i,0], ub[i+2*d,0])
+                ub[i,1] = min(ub[i,1], ub[i+2*d,1])
+            V_i[n] = np.mean(ub[:N-2*k_n[n],0]-ub[:N-2*k_n[n],1])
+
+        X = np.log(k_n)
+        X_m = X - np.mean(X)
+        Y = np.log(V_i)
+        Y_m = Y - np.mean(Y)
+        FDs[ii] = 2 - np.sum((X_m)*(Y_m))/np.sum((X_m)**2)
+    return FDs
+
+compute_fd_serial = numba.njit(compute_fd_parallel.py_func, parallel=False)
+compute_fd_serial_f4 = numba.njit(compute_fd_parallel_f4.py_func, parallel=False)
